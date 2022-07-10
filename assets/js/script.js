612a0e3ac67f3f5bc20e8a6cd95f85f8919a7607
herald-of-spring <anhnguye@usc.edu>
2022-07-09 23:20:58 -0700

    basic javascript randomization applied

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,61 +1,44 @@
-// Comments show the vanilla JavaScript equivalent statements
-
-// var rootEl = document.getElementById("root");
-var rootEl = $('#root');
-
-// var titleEl = document.createElement("h1");
-var titleEl = $('<h1>');
-
-// titleEl.textContent = "Hello friends";
-titleEl.text('Hello friends');
-
-// titleEl.className = 'fancy';
-titleEl.attr('class', 'fancy');
-
-// titleEl.classList.add('p-5') - (`p-5` is for padding)
-titleEl.addClass('p-5');
-
-// titleEl.style.border = "rgb(122, 242, 242) 3px solid";
-titleEl.css('border', 'rgb(122, 242, 242) 5px solid');
-
-// rootEl.appendChild(titleEl);
-rootEl.append(titleEl);
-
-// titleEl.append("Welcome to jQuery");
-rootEl.append('<h2>With jQuery we can:</h2>');
-
-var abilities = [
-  'Select',
-  'Create',
-  'Style',
-  'Animate',
-  'Traverse',
-  'Event Listen',
-  'much more',
-];
-
-for (var i = 0; i < abilities.length; i++) {
-  // Create a new `<div>` for each ability and its text content
-  var abilityEl = $('<li>');
-
-  // abilityEl.textContent = abilities[i];
-  abilityEl.text(abilities[i]);
-
-  // `my-3` class adds margin on top and bottom
-  abilityEl.addClass('my-3');
-
-  // Alternatively
-  // var abilityEl = $("<div>" + abilities[i] + "</div>");
-
-  // Add this new `<div>` to the abilities `<div>` container element.
-  rootEl.append(abilityEl);
+//Using Durstenfeld shuffle to randomize array ordering: used to randomize questions and answers
+function randomize(array) {
+  temp = array.splice(0);
+  for (var i = temp.length; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    [temp[i], temp[j]] = [temp[j], temp[i]];
+  }
+  return temp;
 }
 
-// select all `<div>` elements on the page
-// document.querySelectorAll('div');
-console.log($('div'));
+var questions = [
+  {
+    prompt: "The condition in an if/else statement is enclosed with _____.",
+    options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    correct: "curly brackets",
+  },
+  {
+    prompt: "Commonly used data types in the DOM DO NOT include _____.",
+    options: ["strings", "booleans", "alerts", "numbers"],
+    correct: "alerts",
+  },
+  {
+    prompt: "String values must be enclosed within _____ when being assigned to variables.",
+    options: ["commas", "curly brackets", "quotes", "parentheses"],
+    correct: "quotes",
+  },
+  {
+    prompt: "A very useful tool used during development and debugging for printing content to the debugger is _____.",
+    options: ["JavaScript", "terminal/bash", "\"for\" loops", "console.log"],
+    correct: "console.log",
+  },
+  {
+    prompt: "Arrays in JavaScript can be used to store _____.",
+    options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+    correct: "all of the above",
+  },
+];
 
-// Alternatively use the `$.each` method
-// $.each(abilities, function(i, ability) {
-//    abilityEl.append("<div>" + ability + "</div>");
-// })
+
+
+$("#start-btn").on("click", function() {
+  $("#start-prompt").css("display", "none");
+
+})
