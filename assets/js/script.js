d9a92316a59b083895c784f556b1651e883eb761
herald-of-spring <anhnguye@usc.edu>
2022-07-10 02:41:02 -0700

    formatting and start button javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 //Using Durstenfeld shuffle to randomize array ordering: used to randomize questions and answers
 function randomize(array) {
   temp = array.splice(0);
-  for (var i = temp.length; i > 0; i--) {
+  for (var i = temp.length - 1; i > 0; i--) {
     var j = Math.floor(Math.random() * (i + 1));
     [temp[i], temp[j]] = [temp[j], temp[i]];
   }
@@ -36,9 +36,79 @@ var questions = [
   },
 ];
 
+timeLeft = 75;
+isDone = false;
+hasResponded = false;
 
+function displayQuestions() {
+  var layout = $("<div>").addClass("mx-20");
+  $("body").append(layout);
+  qList = randomize(questions);
+  for (q of qList) {
+    console.log(q.prompt);
+    layout.append($("<h2>").addClass("mx-5").text(q.prompt));
+    console.log(layout);
+    layout.append($("<div>").addClass("mx-5"));
+    //displays each answer, each belonging to class answer-option, and returns value on click
+    aList = randomize(q.options);
+    for (ans of aList) {
+      layout.children().eq(1).append($("<p>").addClass("answer-option").text(ans).on("click", function() {
+        if (this.text === q.correct) {
+          logCorrect();
+        }
+        else {
+          logIncorrect();
+        }
+        hasResponded = true;
+      }));
+    }
+    waitResponse();
+    hasResponded = false;
+    layout.remove();
+  }
+  isDone = true;
+}
+
+function waitResponse() {
+  wait = setInterval(function() {
+    if (hasResponded === true) {
+      clearInterval(wait);
+    }
+  }, 100); 
+}
+
+function logCorrect() {
+  $("#result").attr("class", "text-info").text("Correct!");
+}
+
+function logIncorrect() {
+  $("#result").attr("class", "text-danger").text("Incorrect!");
+  timeLeft -= 10;
+}
+
+function countdown() {
+  timer = setInterval(function() {
+    timeLeft--;
+    $("#time-left").text(timeLeft);
+    if (timeLeft >= 0) {
+      if (isDone && timeLeft > 0) {
+        clearInterval(timer);
+        saveStats(timeLeft);
+      }
+    }
+    if (timeLeft === 0) {
+      clearInterval(timer);
+      saveStats(timeLeft);
+    }
+  }, 1000);
+}
+
+function saveStats() {
+  //do nothing
+}
 
 $("#start-btn").on("click", function() {
   $("#start-prompt").css("display", "none");
-
-})
+  countdown();
+  displayQuestions();
+});
